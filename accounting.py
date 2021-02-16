3e7d0769e57e194ca9489761c1fcd39cc87fff68
Áine Thompson <ainegthompson@gmail.com>
2021-02-16 05:18:08 +0000

    Refactor

--- accounting.py
+++ accounting.py
@@ -29,54 +29,54 @@ def underpaid(filename):
     data = open(filename)
 
     for line in data:
-        tokens = line.rstrip().split('|')
+        _, customer_name, quantity, customer_paid = line.rstrip().split('|')
 
-    customer_id = tokens[0]
-    customer_name = tokens[1]
-    customer_paid = tokens[3]
-    quantity = int(tokens[2])
+        customer_paid = float(customer_paid)
+    
+        customer_expected = float(int(quantity) * melon_cost)
 
-    customer_expected = quantity * melon_cost
+        if customer_expected != customer_paid:
+            print(f"{customer_name} paid ${customer_paid:.2f}, expected ${customer_expected:.2f}")
 
-    if customer_expected != customer_paid:
-        print(f"{customer_name} paid ${customer_paid},", f"expected ${customer_expected}")
-
-
+            if customer_expected > customer_paid:
+                print(f"Underpaid by ${customer_expected - customer_paid:.2f}")
+            elif customer_expected < customer_paid:
+                print(f"Overpaid by ${customer_paid - customer_expected:.2f}")
 
 underpaid("customer-orders.txt")
 
-customer1_expected = customer1_melons * melon_cost
-if customer1_expected != customer1_paid:
-    print(f"{customer1_name} paid ${customer1_paid:.2f},",
-          f"expected ${customer1_expected:.2f}"
-          )
+# customer1_expected = customer1_melons * melon_cost
+# if customer1_expected != customer1_paid:
+#     print(f"{customer1_name} paid ${customer1_paid:.2f},",
+#           f"expected ${customer1_expected:.2f}"
+#           )
 
-customer2_expected = customer2_melons * melon_cost
-if customer2_expected != customer2_paid:
-    print(f"{customer2_name} paid ${customer2_paid:.2f},",
-          f"expected ${customer2_expected:.2f}"
-          )
+# customer2_expected = customer2_melons * melon_cost
+# if customer2_expected != customer2_paid:
+#     print(f"{customer2_name} paid ${customer2_paid:.2f},",
+#           f"expected ${customer2_expected:.2f}"
+#           )
 
-customer3_expected = customer3_melons * melon_cost
-if customer3_expected != customer3_paid:
-    print(f"{customer3_name} paid ${customer3_paid:.2f},",
-          f"expected ${customer3_expected:.2f}"
-          )
+# customer3_expected = customer3_melons * melon_cost
+# if customer3_expected != customer3_paid:
+#     print(f"{customer3_name} paid ${customer3_paid:.2f},",
+#           f"expected ${customer3_expected:.2f}"
+#           )
 
-customer4_expected = customer4_melons * melon_cost
-if customer4_expected != customer4_paid:
-    print(f"{customer4_name} paid ${customer4_paid:.2f},",
-          f"expected ${customer4_expected:.2f}"
-          )
+# customer4_expected = customer4_melons * melon_cost
+# if customer4_expected != customer4_paid:
+#     print(f"{customer4_name} paid ${customer4_paid:.2f},",
+#           f"expected ${customer4_expected:.2f}"
+#           )
 
-customer5_expected = customer5_melons * melon_cost
-if customer5_expected != customer5_paid:
-    print(f"{customer5_name} paid ${customer5_paid:.2f},",
-          f"expected ${customer5_expected:.2f}"
-          )
+# customer5_expected = customer5_melons * melon_cost
+# if customer5_expected != customer5_paid:
+#     print(f"{customer5_name} paid ${customer5_paid:.2f},",
+#           f"expected ${customer5_expected:.2f}"
+#           )
 
-customer6_expected = customer6_melons * melon_cost
-if customer6_expected != customer6_paid:
-    print(f"{customer6_name} paid ${customer6_paid:.2f},",
-          f"expected ${customer6_expected:.2f}"
-          )
+# customer6_expected = customer6_melons * melon_cost
+# if customer6_expected != customer6_paid:
+#     print(f"{customer6_name} paid ${customer6_paid:.2f},",
+#           f"expected ${customer6_expected:.2f}"
+#           )
